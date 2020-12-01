d43914679b50dd560c41b9a8b1a08b8b1824fa99
邵謙(Shau Chien) <cvcvbnbn13@gmail.com>
2020-12-01 11:20:02 +0800

    new from 12/1

--- 3d-card-effect-master/app.js
+++ 3d-card-effect-master/app.js
@@ -2,10 +2,10 @@
 const card = document.querySelector(".card");
 const container = document.querySelector(".container");
 const title = document.querySelector(".title");
-const sneaker = document.querySelector(".sneaker img");
-const purchase = document.querySelector(".purchase button");
+const headPhoto = document.querySelector(".headPhoto img");
+const follow = document.querySelector(".follow button");
 const description = document.querySelector(".info h3");
-const sizes = document.querySelector(".sizes");
+const traits = document.querySelector(".traits");
 // items
 
 
@@ -20,20 +20,20 @@ container.addEventListener('mouseenter',e=>{
   card.style.transform = "none";
   // popout
   title.style.transform = "translateZ(150px)"
-  sneaker.style.transform = "translateZ(200px) rotateZ(-5deg)"
+  headPhoto.style.transform = "translateZ(200px) rotateZ(-5deg)"
   description.style.transform = "translateZ(150px)"
-  sizes.style.transform = "translateZ(150px)"
-  purchase.style.transform = "translateZ(150px)"
-})
+  traits.style.transform = "translateZ(150px)"
+  follow.style.transform = "translateZ(150px)"
+});
 // animate out
 container.addEventListener('mouseleave',e=>{
   card.style.transition = "all 0.5s ease";
   card.style.transform = `rotate(0deg)`;
   // popback
   title.style.transform = "translateZ(0px)"
-  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)"
+  headPhoto.style.transform = "translateZ(0px) rotateZ(0deg)"
   description.style.transform = "translateZ(0px)"
-  sizes.style.transform = "translateZ(0px)"
-  purchase.style.transform = "translateZ(0px)"
+  traits.style.transform = "translateZ(0px)"
+  follow.style.transform = "translateZ(0px)"
+});
 
-})
--- RPS/script.js
+++ RPS/script.js
@@ -26,17 +26,20 @@ selectionButtons.forEach((button)=>{
         const selectionName = button.dataset.selection;
         // 在html屬性用data設置的值
         // js用dataset+data名來獲取
-        // rock paper scissors
+        // selectonName可能是 rock paper scissors
         const selection = SELECTIONS.find(selection => selection.name === selectionName)
-        // 這裡的selection = 去陣列裡面找跟你選的一樣的name
+        // 這裡的selection = 去陣列裡面找跟你選的一樣的name 然後回傳第一個滿足條件的元素值
         makeSelection(selection);
-        // SELECTIONS[0],[1],[2]其中一個 人選的
+        // SELECTIONS[0],[1],[2]其中一個array 
+        // 人選的
+        // 記得注意selection是array啊啊啊啊
     });
 });
 
 function makeSelection(selec1){
     const computerSelection = randomSelection();
-    // SELECTIONS[0],[1],[2]其中一個 電腦選的
+    // SELECTIONS[0],[1],[2]其中一個 
+    // 電腦選的
     const peopleWinner = isWinner(selec1, computerSelection);
     // 人選的beats === 電腦選的name
     const computerWinner = isWinner(computerSelection, selec1);
@@ -49,8 +52,17 @@ function makeSelection(selec1){
     if(computerWinner) calcuScore(computerScore);
 }
 
+function randomSelection(){
+    const randomIndex = Math.floor(Math.random()*SELECTIONS.length)
+    // .floor() 函式會回傳小於等於所給數字的最大整數。
+    // SELECTIONS的長度為3
+    return SELECTIONS[randomIndex];
+    // return SELECTIONS[0]OR[1]OR[2]
+}
+
 function isWinner(yourSelection , opponentSelection){
     return yourSelection.beats === opponentSelection.name;
+    // return ture false
 }
 
 
@@ -59,9 +71,12 @@ function selectionResult(selec2, winner){
     const div = document.createElement('div');
     div.innerText = selec2.emoji ;
     div.classList.add('result-selection');
+    // opacity .5
     if(winner){
         // 如果beats===name
         div.classList.add('winner');
+        // opacity: 1;
+        // font-size: 2rem;
     }
     finalColumn.after(div)
 
@@ -69,9 +84,4 @@ function selectionResult(selec2, winner){
 
 function calcuScore(score) {
     score.innerText = parseInt(score.innerText) +1;
-}
-
-function randomSelection(){
-    const randomIndex = Math.floor(Math.random()*SELECTIONS.length)
-    return SELECTIONS[randomIndex];
 }
